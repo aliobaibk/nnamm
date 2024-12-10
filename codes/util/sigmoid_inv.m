d559c9c1b9b36d0b0d1945093fabf64f8718c40f
aliobaibk <43523316+aliobaibk@users.noreply.github.com>
2024-12-10 11:01:07 -0500

    Improve sigmoid_inv.m to handle vector inputs

--- codes/util/sigmoid_inv.m
+++ codes/util/sigmoid_inv.m
@@ -1,4 +1,4 @@
 function X = sigmoid_inv(Y, s_max, s_rate, s_mode, s_off)
-X = s_mode - log(s_max ./ (Y - s_off) - 1) / s_rate;
-X(~is_in([s_off, s_off + s_max], '<', Y, '<')) = NaN;
+X = s_mode - log(s_max ./ (Y - s_off) - 1) ./ s_rate;
+X((s_off >= Y) | (Y >= s_off + s_max)) = NaN;
 end
